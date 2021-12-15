4b2b6d11ed84c306c1a1d245586a324b6acbd793
Kawa <victor.borruat@hotmail.com>
2021-12-15 17:41:47 +0100

    added log attempt

--- src/GQJacobi/gauss-log.hpp
+++ src/GQJacobi/gauss-log.hpp
@@ -105,7 +105,7 @@ namespace GQLog {
             ab(0,1)=mom[0];
             std::cout << " so far so good 1" << std::endl;
 
-            std::cout << size(mom) <<std::endl;
+            std::cout << mom.size() <<std::endl;
             sig.col(1) = mom;
 
             std::cout << " so far so good 2" << std::endl;
