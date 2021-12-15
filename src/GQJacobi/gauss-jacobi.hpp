7aa8b52907d5e01a34829c64e92149ddb9f45e17
Kawa <victor.borruat@hotmail.com>
2021-12-15 14:36:11 +0100

    added = operator

--- src/GQJacobi/gauss-jacobi.hpp
+++ src/GQJacobi/gauss-jacobi.hpp
@@ -88,7 +88,8 @@ namespace GQJacobi{
         GaussJacobiRule& operator=(const GaussJacobiRule& gq){
             if(this != &gq){
                 this->degree = gq.degree;
-                std::cout << "copied degree" << std::endl;
+                std::cout << "copied degree to "<< this->degree << std::endl;
+                
                 for(size_t i = 0; i < degree; i++){
                     std::cout << "copying " << i << "th nodes / weight" << std::endl;
                     this->nodes.push_back(gq.nodes[i]);
