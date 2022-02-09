b9245520a78d610b8bf8544545f7020a176707da
Kawa <victor.borruat@hotmail.com>
2022-02-09 14:39:10 +0100

    chebyshev update

--- src/GQJacobi/gauss-log.hpp
+++ src/GQJacobi/gauss-log.hpp
@@ -185,7 +185,7 @@ namespace GQLog {
                 ab(k, 1) = sigma(k,k)/sigma(k-1,k-1);
             }
 
-            std::cout << sig << std::endl;
+            std::cout << sigma << std::endl;
 
             return ab;
         }
