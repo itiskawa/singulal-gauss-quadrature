ad91d223d8bf72a254c7da5701ffeafac9c6d00f
Kawa <victor.borruat@hotmail.com>
2022-03-03 11:02:32 +0100

    added example code

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <SingGQ/gauss-jacobi.hpp>
+#include <SingGQ/gauss-log.hpp>
+
+
 using namespace std;
 
-#include <eigen3/Eigen/Dense>
-#include "NumCpp.hpp"
-#include <cstdlib>
 
 
 int main(){
 
-    Eigen::MatrixXd a = Eigen::MatrixXd::Random(3,3);
-    cout << a << endl;
+    GQJacobi::GaussJacobiRule<double> gq(10, -0.3, -0.7); // 10-point Gauss-Jacobi quadrature rule with alpha = -0.3 and beta = -0.7
+
+    double jacobiSinInteg = gq([](double x){return sin(x);}); // evaluating the weighted sin(x) over ]-1,1[
+
+    double shiftedJacobiSin = gq([](double x){return sin(x);}, 0, 2); // same integral but over ]0,2[
 
     return 0;
 }
